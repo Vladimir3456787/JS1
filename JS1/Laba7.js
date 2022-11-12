d27ec57a19a95b5bb90010810c30304082c89df9
Vladimir3456787 <100952199+Vladimir3456787@users.noreply.github.com>
2022-11-12 18:58:19 +0700

    Add files via upload

---
+++ JS1/Laba7.js
@@ -0,0 +1,30 @@
+var Name;
+Name="Приступаем к работе";
+console.log(Name);
+
+
+let a = 3;
+let c = 2;
+console.log('a=',a,'c=',c);
+
+console.log('a+c=', a+c);
+
+console.log(Name.toUpperCase());
+console.log(Name.toLowerCase());
+
+console.log('a*c=', a*c);
+console.log('a-c=', a-c);
+console.log('a/c=', a/c);
+console.log('c^2=', Math.pow(c,2));
+console.log('a % c=', a % c);
+
+//Задание 1 Найти гипотенузу прямоугольного треугольника//
+let kat = 12;
+let kat2 = 5;
+console.log('Гипотенуза =', gip = Math.sqrt(Math.pow(kat,2)+Math.pow(kat2,2)));
+
+
+//задание 2 Сила тяжести//
+let g = 9.8;
+let m = 10;
+console.log("Сила тяжести =", F = g*m, "H");            
